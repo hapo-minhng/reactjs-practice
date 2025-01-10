2709d40a8dc00ac15dbce9a5de1a143739cab1e5
Minh Nguyen <minhng@haposoft.com>
2025-01-10 16:49:24 +0700

    Working with Fragments

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -27,7 +27,7 @@ function App() {
   }
 
   return (
-    <div>
+    <>
       <Header />
       <main>
         <section id="core-concepts">
@@ -77,7 +77,7 @@ function App() {
           {tabContent}
         </section>
       </main>
-    </div>
+    </>
   );
 }
 
