414594ad9a7a57cca054faffb030c1d754b0aaec
Minh Nguyen <minhng@haposoft.com>
2025-01-13 15:44:26 +0700

    Lifting State Up [Core Concept]

--- 07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -6,32 +6,38 @@ const initialGameBoard = [
   [null, null, null],
 ];
 
-export default function GameBoard() {
-    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
-    function handleSelectSquare(rowIndex, colIndex) {
-        setGameBoard((prevGameBoard) => {
-            const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-            updatedBoard[rowIndex][colIndex] = "X";
-            return updatedBoard;
-        });
-    }
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+      return updatedBoard;
+    });
+      
+      onSelectSquare();
+  }
 
   return (
     <ol id="game-board">
       {gameBoard.map((row, rowIndex) => (
-          <li key={rowIndex}>
-              <ol>
-                  {row.map((playerSymbol, colIndex) => (
-                      <li key={colIndex}>
-                          <button onClick={function () {
-                              handleSelectSquare(rowIndex, colIndex)
-                          }}>
-                              {playerSymbol}
-                          </button>
-                      </li>
-                  ))}
-              </ol>
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button
+                  onClick={function () {
+                    handleSelectSquare(rowIndex, colIndex);
+                  }}
+                >
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
         </li>
       ))}
     </ol>
