da49c007c2c6dd6c6779bca5c0500c0b5f36816d
Minh Nguyen <minhng@haposoft.com>
2025-01-13 11:54:15 +0700

    Best Practice: Updating State Based On Old State Correctly

--- 07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/PlayerInfo.jsx
+++ 07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/PlayerInfo.jsx
@@ -4,7 +4,7 @@ export default function Player({ name, symbol }) {
   const [isEditting, setIsEditting] = useState(false);
 
   function handleEdit() {
-    setIsEditting(!isEditting);
+    setIsEditting((editing) => !editing);
   }
 
   let playerName = <span className="player-name">{name}</span>;
